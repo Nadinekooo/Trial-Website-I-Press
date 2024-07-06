c96f4f56a5dd71f036ad7f3eb308793f2a282799
Nadinekooo <165750392+Nadinekooo@users.noreply.github.com>
2024-07-06 17:11:29 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <h3>PRESENSI MAHASISWA</h3>
+        <a href="">Login</a> 
+    </header> 
+
+
+    <footer>
+        <hr />
+        <i>Terimakasih telah mengakses</i>
+    </footer>
+
+    <form>
+        <input type="text" placeholder="username" name="username"/>
+        <input type="text" placeholder="password" name="password"/>
+        <input type="text" placeholder="prodi" name="prodi"/>
+        <buttontype="submit"> hadir</button>
+    </form>
+
+    <main> 
+        <p>Selamat datang di IPress</p>
+    </main>
+
+    
+</body>
+</html>
